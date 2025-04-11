aef1e1d6b884bdddb4f38a53f6aff958708050bc
divyasreebollu <141166868+divyasreebollu@users.noreply.github.com>
2025-04-11 16:13:48 +0530

    Delete Android Project/BusTrackingFee/app directory

--- Android Project/BusTrackingFee/app/src/main/java/com/feesBus/dataLayer/response/CommonResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.feesBus.dataLayer.response
-
-import com.google.gson.annotations.SerializedName
-
-data class CommonResponse(
-    @SerializedName("error") var error: Boolean? = null,
-    @SerializedName("message") var message: String? = null,
-)
