d251064b038bbcb083de612301861be9ee80e49b
divyasreebollu <141166868+divyasreebollu@users.noreply.github.com>
2025-04-11 16:14:00 +0530

    Delete Android/BusTrackingFee/app directory

--- Android/BusTrackingFee/app/src/main/java/com/feesBus/ui/users/models/PointerPoint.kt
+++
@@ -1,9 +0,0 @@
-package com.feesBus.ui.users.models
-
-import androidx.annotation.DrawableRes
-
-data class PointerPoint(
-    @DrawableRes val image: Int,
-    val head: String,
-    val body: String,
-)
